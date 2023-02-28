5bf54b70426d19fb89cbe42e88a8ee800c0794e8
Bernardo Pinheiro <111240182+berpinheiroo@users.noreply.github.com>
2023-02-27 21:16:05 -0300

    Integrando o validator no leitor de planilhas

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const readXlsxFile = require('read-excel-file/node')
+const validator = require('validator')
 
 readXlsxFile('Alunos.xlsx').then((rows)=> {
+    console.log('-----------------------')
+    console.log('         Alunos        ')
+    console.log('-----------------------')
+    for (let i = 1; i < rows.length; i++) {
+        let validarNome = validator.isEmpty(rows[i][0])
+        let validarEmail = validator.isEmail(rows[i][1])
+        let validarIdade
+        if (rows[i][2] > 0) {
+            validarIdade = true
+        }
+        else {
+            validarIdade = false
+        }
+        let validarMatricula = validator.isAlphanumeric(rows[i][3])
 
-    for (let i = 0; i < 4; i++) {
-        console.log('Nome: ' + rows[i][0])
-        console.log('E-mail: ' + rows[i][1])
-        console.log('Idade: ' + rows[i][2])
-        console.log('Matrícula: ' + rows[i][3])
-        console.log('-----------------------')
+        if (validarNome === false && validarEmail === true && validarIdade === true && validarMatricula === true) {
+            console.log('Nome: ' + rows[i][0])
+            console.log('E-mail: ' + rows[i][1])
+            console.log('Idade: ' + rows[i][2])
+            console.log('Matrícula: ' + rows[i][3])
+            console.log('-----------------------')
+        }
     }
 })
